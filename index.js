863d4a9135c4a1c683c2cf481cb02c42915d07b1
Nichelle Rivera <nichelleriv@gmail.com>
2019-03-20 09:30:44 -0500

    WE GOT THIS x2

---
+++ index.js
@@ -0,0 +1,10 @@
+// Getting a reference to the button on the page with the id property set to `click-me`
+var button = d3.select("#glycine");
+
+
+button.on("click", function() {
+  d3.select(".glycinepic").html("<img src='Glycine.svg' alt='giphy'>");
+  console.log("I am working!")
+});
+
+
